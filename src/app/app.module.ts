67e59e9ef351b8b9ecc97340acaf741acffcb76e
Gache <erickfrancodelgado@hotmail.com>
2020-05-13 09:59:39 +0200

    Création des composants et installation de Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtisteComponent } from './components/artiste/artiste.component';
+import { MenuComponent } from './components/shared/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtisteComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
