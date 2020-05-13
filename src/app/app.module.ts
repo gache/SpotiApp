904053c1eab2cf0daff7ab9c21f9b932d75d5444
Gache <erickfrancodelgado@hotmail.com>
2020-05-13 10:38:19 +0200

    Création des routes et du menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'; // pour importer mes routes  je dois importer  le routerModule
+
+// Importation Routes
+import { ROUTES } from './app.routes'; // Importation de ma route
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtisteComponent } from './components/artiste/artiste.component';
 import { MenuComponent } from './components/shared/menu/menu.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -13,10 +18,12 @@ import { MenuComponent } from './components/shared/menu/menu.component';
     HomeComponent,
     SearchComponent,
     ArtisteComponent,
-    MenuComponent
+    MenuComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES, { useHash: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
