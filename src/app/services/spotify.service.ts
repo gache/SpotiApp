de741cc0432363f8b64e4cd6d03e8be5000f7277
Gache <erickfrancodelgado@hotmail.com>
2020-05-21 18:36:48 +0200

    je fais plus de precision avec les commentaire de mon service

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 // la petition a besoin de header je fais l'importation.
 
-import {map} from 'rxjs/operators';  // importation  de l'operator map
+import { map } from 'rxjs/operators';  // importation  de l'operator map
 // operator map filtre l'information que je souhaite montrer.
 // operator map travaille avec observables.
 
@@ -11,37 +11,40 @@ import {map} from 'rxjs/operators';  // importation  de l'operator map
 })
 export class SpotifyService {
   // j'injecte le module HttpClient à mon contructor pour y acceder aux propriétés et methodes du module
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  getQuery( query: string) {
-  // comme l'adresse url de ma petition change à partir de slash, je la centralise dans une constante l'Url de l'Api pour de nouvelle version future
-    const URL = `https://api.spotify.com/v1/${ query }`;
+  getQuery(query: string) {
+    // comme l'adresse url de ma petition change à partir de slash, je la centralise dans une constante l'Url de l'Api pour de nouvelle version future
+    const URL = `https://api.spotify.com/v1/${query}`;
 
-  // cette Api a besoin du token ou une autentification pour envoyer l'information
-  // dans notre cas on ava uitiliser l'headers avec une autentification
-    const headers = new HttpHeaders ({
-    // ici je mets tous les header que l'Api a besoin
-    Authorization: 'Bearer BQBD77ElDiVB0CEeGZqV9B1BuraiXj1gvGLlw9BYpEKHOOjdsD__mpXkAmqnRX0hOlKZnXTLucir_SRBaJM'
-  });
- 
-    return this.http.get( URL, { headers} );
+    // cette Api a besoin du token ou une autentification pour envoyer l'information
+    // dans notre cas on ava uitiliser l'headers avec une autentification
+    const headers = new HttpHeaders({
+      // ici je mets tous les header que l'Api a besoin
+      Authorization: 'Bearer BQDqGPYYGglGgE0GfQHMfitIa4ecZ2PmpdxU7so9Fv1Qd5nqzT6SgJyNdIRzh7lkiHLEe4-ddpGqjJBG63Y'
+    });
+    // ja fais fais al petition get j'envoie url et les headers
+    return this.http.get(URL, { headers });
   }
 
-  // petition get avoir la liste de chanson la plus écouter
+  // petition get avoir les albums le plus écouter
   getNewRelease() {
+
+    // je lui dis à ma function getNewRelease() qu'elle doit utiliser la function getQuery() pour avoir l'url et les headers. et j'envoie l'argument qui va être query de ma function getQuery()
     return this.getQuery('browse/new-releases?limit=20')
-               .pipe( map( data => {
-               return data['albums'].items;
-   // pour utiliser operator map j'utilise un pipe avec l'operator map pour filtrer l'information de l'Api
-   }));
+      .pipe(map(data => {
+        return data['albums'].items; // c'est la route du tableau de l'api de spotify
+        // pour utiliser operator map j'utilise un pipe avec l'operator map pour filtrer l'information qui viens l'Api
+      }));
   }
 
   // petition get pour avoir l'artiste de mamnière individuelle
   getArtist(terme: string) {
-  return this.getQuery(`search?q=${ terme }&type=artist&limit=15 `)
-             .pipe(map( data => {
-             return data ['artists'].items;
- }));
+    return this.getQuery(`search?q=${terme}&type=artist&limit=15 `)
+      .pipe(map(data => {
+        return data['artists'].items; // c'est la route du tableau de l'api de spotify
+        // pour utiliser operator map j'utilise un pipe avec l'operator map pour filtrer l'information qui viens l'Api
+      }));
 
   }
 }
