2f2ee067dc6a79c094455f4f5b8f17e442e9be98
Gache <erickfrancodelgado@hotmail.com>
2020-05-22 10:57:56 +0200

    route de l'artist problème avec Id

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
+import { ArtisteComponent } from './components/artiste/artiste.component';
 
 //  creation des routes
 export const ROUTES: Routes = [
 
   { path: 'home', component: HomeComponent },
   { path: 'search', component: SearchComponent },
+  { path: 'artiste/:id', component: ArtisteComponent },
   { path: ' ', pathMatch: 'full', redirectTo: 'home' },
   { path: '** ', pathMatch: 'full', redirectTo: 'home' }
 ]
--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cards',
@@ -9,9 +10,24 @@ export class CardsComponent implements OnInit {
 
   @Input() chansons: any[] = [];
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+  // avoir l'artiste avec son Id
+  voirArtiste(chanson: any) {
+
+    let artisteId;
+    if (chanson.terme === 'artist') {
+      artisteId = chanson.id;
+    } else {
+      artisteId = chanson.artists[0].id;
+    }
+
+    this.router.navigate(['/artiste', artisteId])
+    // console.log(artisteId);
+
+
   }
 
 }
